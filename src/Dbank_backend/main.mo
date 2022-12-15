7333e56e4df71905dec5e8e08649cd33cfaffe91
RyuTsuki08 <ryudetsuki1587@gmail.com>
2022-12-15 13:23:22 -0400

    feat and style: culminate back and structure front

--- src/Dbank_backend/main.mo
+++ src/Dbank_backend/main.mo
@@ -1,7 +1,39 @@
 import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+import Principal "mo:base/Principal";
 
 actor Dbank{
-  var currentValue = 300;
+  stable var currentValue: Float = 300;
+  stable var startTime = Time.now();
 
-  Debug.print("Hello");
+  public func topUp(amount: Float){
+    currentValue += amount;
+  };
+
+  public func retire(amount: Float){
+    let compareValue: Float = currentValue - amount;
+    if(compareValue >= 0)
+    {
+    currentValue -= amount;
+    }else{
+      Debug.print("I\'m sorry, insufficient balance");
+    }
+  };
+
+  public query func checkBalance(): async Float{
+    return currentValue;
+  };
+
+  public func compound(){
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+
+    startTime := currentTime;
+  };
+
+
 }
--- src/Dbank_frontend/src/index.js
+++ src/Dbank_frontend/src/index.js
@@ -1,19 +1,62 @@
-import { Dbank_backend } from "../../declarations/Dbank_backend";
+import { Dbank_backend } from '../../declarations/Dbank_backend';
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const button = document.getElementById('plug-connect-btn');
+const principalId = document.getElementById('principal-id');
 
-  const name = document.getElementById("name").value.toString();
+async function checkConnection(){
+  const result = await window.ic.plug.isConnected()
+  const plugId = window.ic.plug.sessionManager.sessionData;
+  if(result){
+    principalId.innerText = `Welcome ${plugId.principalId}`;
+  }
+}
 
-  button.setAttribute("disabled", true);
+async function PlugConnect(){
+  try {
+    let publicKey = await window.ic.plug.requestConnect();
+    console.log(`The connected user's public key is:`, publicKey);
+    const plugID = window.ic.plug.sessionManager.sessionData;
+    console.log(plugID)
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await Dbank_backend.greet(name);
+button.addEventListener('click', PlugConnect)
 
-  button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+async function checkBalance(){
+  const currentAmount = await Dbank_backend.checkBalance();
 
-  return false;
+  const span = document.getElementById('value');
+  span.innerText = currentAmount.toFixed(2);
+}
+
+window.addEventListener('load', checkBalance);
+window.addEventListener('load', checkConnection);
+
+document.querySelector('form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  let inputTopTup = document.getElementById('input-amount').value;
+  let inputRetire = document.getElementById('withdrawal-amount').value;
+  const button = event.target.querySelector('#submit-btn')
+
+  if(inputTopTup){
+    button.setAttribute('disabled', true)
+    await Dbank_backend.topUp(parseFloat(inputTopTup))
+    inputTopTup = '';
+    button.removeAttribute('disabled')
+    checkBalance();
+  }
+
+  if(inputRetire){
+    button.setAttribute('disabled', true)
+    await Dbank_backend.retire(parseFloat(inputRetire))
+    inputRetire = '';
+    button.removeAttribute('disabled')
+    checkBalance();
+  } 
+
+  // await Dbank_backend.compound();
+
 });
